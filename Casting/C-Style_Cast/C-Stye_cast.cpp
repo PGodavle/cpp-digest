c4fe71e0991dd7cebd848519a4330b33b375b13e
Parikshit Godavle <parikshitgodavle@gmail.com>
2025-06-14 11:59:34 +0530

    Create C-Stye_cast.cpp

---
+++ Casting/C-Style_Cast/C-Stye_cast.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 65;
+    char c = (char)i; // C-style cast converting int to char.
+    cout << "Character: " << c << endl; // Outputs: A
+    return 0;
+}
